54c68a7d6ae2f5df890f3eadf638cd0b985eeb4f
williamjameswillis <77491334+williamjameswillis@users.noreply.github.com>
2025-12-09 11:54:23 +0000

    add coverage to sonarcloud analysis

--- scripts/tests/unit.sh
+++ scripts/tests/unit.sh
@@ -9,5 +9,9 @@ mkdir -p test-artefacts
 
 echo "Running unit tests..."
 poetry run pytest src/ -v \
+  --cov="." \
+  --cov-report=html:test-artefacts/coverage-html \
+  --cov-report=xml:test-artefacts/coverage.xml \
+  --cov-report=term \
   --junit-xml="test-artefacts/unit-tests.xml" \
   --html="test-artefacts/unit-tests.html" --self-contained-html
